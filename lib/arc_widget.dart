9611e143a88310a8f2ff53f703623d6e4aa148dc
David <quintanilladtona@gmail.com>
2024-09-02 17:12:50 +0200

    canvis arc

--- lib/arc_widget.dart
+++ lib/arc_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
+import 'numerology_calculation.dart';
 
 class ArcWidget extends StatelessWidget {
   final Map<String, int> values;
@@ -14,7 +15,7 @@ class ArcWidget extends StatelessWidget {
           width: constraints.maxWidth,
           height: constraints.maxWidth, // Mantenim un widget quadrat
           child: CustomPaint(
-            painter: ArcPainter(values: values),
+            painter: ArcPainter(values: values, maxWidth: constraints.maxWidth, maxHeight: constraints.maxHeight),
           ),
         );
       },
@@ -24,8 +25,10 @@ class ArcWidget extends StatelessWidget {
 
 class ArcPainter extends CustomPainter {
   final Map<String, int> values;
+  final double maxWidth;
+  final double maxHeight;
 
-  ArcPainter({required this.values});
+  ArcPainter({required this.values, required this.maxWidth, required this.maxHeight});
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -36,7 +39,7 @@ class ArcPainter extends CustomPainter {
 
     // Defineix el centre de la fletxa
     final center = Offset(size.width / 2, size.height / 2);
-    final radius = min(size.width, size.height) / 2 -60;
+    final radius = min(size.width, size.height) / 2 - 60;
 
     // Dibuixa la fletxa
     final arrowStart = Offset(center.dx, center.dy + radius); // Inici de la fletxa
@@ -57,34 +60,62 @@ class ArcPainter extends CustomPainter {
     final Rect arcRect = Rect.fromCircle(center: arcCenter, radius: arcRadius);
     canvas.drawArc(arcRect, pi, pi, false, paint);
 
-     // Mostrar els textos utilitzant el `Map`
+    // Ajustar el mides del text en funció del contenidor
+    final double textSize = min(maxWidth, maxHeight) / 17; // Ajusta el divisor per controlar la mida del text
+
+    // Crear TextStyle amb mida ajustada
+    final TextStyle titleStyle = TextStyle(color: Colors.black, fontSize: textSize, fontWeight: FontWeight.normal);
+    final TextStyle valueStyle = TextStyle(color: Colors.black, fontSize: textSize, fontWeight: FontWeight.bold);
+
+    // Mostrar els textos utilitzant el `Map`
     if (values.length >= 4) {
       // Primers i segons valors per als extrems del semicercle
       final arcLeft = Offset(arcCenter.dx - arcRadius, arcCenter.dy);
       final arcRight = Offset(arcCenter.dx + arcRadius, arcCenter.dy);
-      _drawText(canvas, "${values.keys.elementAt(0)}", values.values.elementAt(0), arcLeft + Offset(0, 20));
-      _drawText(canvas, "${values.keys.elementAt(1)}", values.values.elementAt(1), arcRight + Offset(1, 20));
+      _drawText(canvas, "${values.keys.elementAt(0)}", values.values.elementAt(0), arcLeft + Offset(0, 20), titleStyle, valueStyle);
+      _drawText(canvas, "${values.keys.elementAt(1)}", values.values.elementAt(1), arcRight + Offset(10, 20), titleStyle, valueStyle);
 
       // Tercer i quart valors per als extrems de la fletxa
-      _drawText(canvas, "${values.keys.elementAt(2)}", values.values.elementAt(2), arrowEnd + Offset(0, -30));
-      _drawText(canvas, "${values.keys.elementAt(3)}", values.values.elementAt(3), arrowStart + Offset(0, 20));
+      _drawText(canvas, "${values.keys.elementAt(2)}", values.values.elementAt(2), arrowEnd + Offset(0, -30), titleStyle, valueStyle);
+      _drawText(canvas, "${values.keys.elementAt(3)}", values.values.elementAt(3), arrowStart + Offset(0, 20), titleStyle, valueStyle);
     }
   }
 
-  void _drawText(Canvas canvas, String key, int value, Offset offset) {
-    final TextStyle titleStyle = TextStyle(color: Colors.black, fontSize: 14, fontWeight: FontWeight.normal);
-    final TextStyle valueStyle = TextStyle(color: Colors.black, fontSize: 14, fontWeight: FontWeight.bold);
+  void _drawText(Canvas canvas, String key, int value, Offset offset, TextStyle titleStyle, TextStyle valueStyle) {
+    final reducedValue = reduceToSingleDigit(value);
+    final bool masterNumber = isMasterNumber(reducedValue);
 
     final TextSpan titleSpan = TextSpan(text: key, style: titleStyle);
-    final TextSpan valueSpan = TextSpan(text: '\n$value', style: valueStyle);
+    final TextSpan valueSpan = TextSpan(
+      text: masterNumber ? '\n$reducedValue/${reduceToSingleDigitResult(reducedValue)}' : '\n$reducedValue',
+      style: masterNumber
+          ? valueStyle.copyWith(color: Colors.red, backgroundColor: Colors.yellow) // Resaltar números mestres
+          : valueStyle,
+    );
 
-    final TextPainter textPainter = TextPainter(
-      text: TextSpan(children: [titleSpan, valueSpan]),
+    // Crear TextPainter per al títol
+    final TextPainter titlePainter = TextPainter(
+      text: titleSpan,
       textDirection: TextDirection.ltr,
     );
-    
-    textPainter.layout(minWidth: 0, maxWidth: double.maxFinite);
-    textPainter.paint(canvas, offset - Offset(textPainter.width / 2, textPainter.height / 2));
+    titlePainter.layout();
+
+    // Crear TextPainter per al valor
+    final TextPainter valuePainter = TextPainter(
+      text: valueSpan,
+      textDirection: TextDirection.ltr,
+    );
+    valuePainter.layout();
+
+    // Calcular l'offset per centrar el text
+    final double xOffset = offset.dx - max(titlePainter.width, valuePainter.width) / 2;
+    final double yOffset = offset.dy;
+
+    // Dibuixar el text títol
+    titlePainter.paint(canvas, Offset(xOffset, yOffset - (titlePainter.height / 2) - (valuePainter.height / 2) + 10));
+
+    // Dibuixar el text del valor centrat amb el text títol
+    valuePainter.paint(canvas, Offset(xOffset+7, yOffset + (titlePainter.height / 2) - 25));
   }
 
   @override
